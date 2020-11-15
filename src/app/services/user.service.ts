6e74918b1c258ad7dee6fb05f4cf932878f1741b
Pierre <pierreriviere@hotmail.com>
2020-11-15 12:13:25 +0100

    ⚡️ Add User api service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { User } from '../models/user.interface';
+
+const API_URL = environment.gmgBackApiUrl;
+
+@Injectable()
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Get all users from back api
+   
+   * @returns {Observable<*>}
+   */
+  public getAllUsers(): Observable<User[]> {
+    return this.http.get(`${API_URL}/users`).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+}
